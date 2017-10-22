6392a088cb660181f9d375f10ecd333e3e4cbabd
Telega815 <telega815@gmail.com>
2017-10-22 19:57:38 +0300

    user input

--- Scripts.js
+++ Scripts.js
@@ -1,46 +1,72 @@
  /**
-   * Функция рачета стоимости
-   * amount - количество человек, должно быть целочисленное и больше 0, валидации нет!!!
-   * isEvening - булеан вечернее время
-   * isRomantic - булеан романтик щет
-   * form - форма для вывода результата (необязательно, тк функция возвращает результат)
+   * 
    */
-function calculate(amount, isEvening, isRomantic, form){
+function changeResultRomanting(amount){
 	var result;
+	var slider = document.getElementById("amountSlider");
+	var selectList = document.getElementById("selectList");
 	var checkbox = document.getElementById("cbCustom");
-	var outputAmount = document.getElementById('outputAmount')
-	changeColor(amount);
-	outputAmount.value = amount.value;
+	var outputCounter = document.getElementById('outputCounter');
+	var outputResult = document.getElementById("outputResult");
 
-	if (isRomantic){
-		isEvening.disable;
-		amount.value =2;
-		changeColor(amount);
-		outputAmount.value = amount.value;
-		amount.disable;
+	if (document.getElementById("romantingCheckbox").checked){
+		selectList.disable;
+		slider.value = 2;
+		outputCounter.value = slider.value;
+		changeColor(slider);
+		slider.disable;
 		checkbox.style.background = "#6e93ff";
-		if (form != null) form.value = 3 + " " + 500 + " руб.";
-		return 3 + " " + 500;
+		outputResult.value = 3 + " " + 500 + " руб.";
+	}else{
+		changeResult();
 	}
+	
+}
 
+/**
+   * 
+   */
+function changeResult(){
+	if (document.getElementById("romantingCheckbox").checked) {
+		changeResultRomanting();
+		return;
+	}
+	var result;
+	var amount;
+	var isEvening;
+	var slider = document.getElementById("amountSlider");
+	var selectList = document.getElementById("selectList");
+	var checkbox = document.getElementById("cbCustom");
+	var outputCounter = document.getElementById('outputCounter');
+	var outputResult = document.getElementById("outputResult");
+
+	
+
+	changeColor(slider);
 	checkbox.style.background = "#212121";
+	amount = slider.value;
+	outputCounter.value = amount;
 
-	amount = amount.value;
-	if(isEvening.selectedIndex == 0) isEvening = false;
+	if(selectList.selectedIndex == 0) isEvening = false;
 	else isEvening = true;
 
 	result = (amount < 5 ? 0 : amount -5) * 100 + 500;
 	result = isEvening ? result*2 : result;
 
-	if (form != null) form.value = trim(result) + " руб.";
-	return result;
+	outputResult.value = trim(result) + " руб.";
 }
 
+/**
+   * 
+   */
 function changeColor(slider){
 	var res = ((slider.value-1)/11)*100;
 	slider.style.background = "linear-gradient(to right, #6e93ff " + res +"%, #d3d3d3 "+res+"%)";
 }
 
+/**
+   * 
+   */
 function trim(a){
 	if (a > 999){
 		return Math.floor(a/1000) + " " + ((a % 1000 != 0) ? (a % 1000) : "000") ;
@@ -48,11 +74,3 @@ function trim(a){
 	else return a;
 }
 
-function clearSelection() {
-    if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-    } else { // старый IE
-      document.selection.empty();
-    }
-    alert("finished");
-  }
